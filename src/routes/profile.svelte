e1b634e82f501825da36c056ff5822793b8b19df
Alejandro Sáez <alejandro.sapa@gmail.com>
2020-07-17 19:32:38 -0400

    Agregue video

--- src/routes/profile.svelte
+++ src/routes/profile.svelte
@@ -10,4 +10,7 @@
 <div class="Profile container">
   <h1>Seccion del perfil</h1>
   <Profile/>
+  <div class="mt-4" >
+  <iframe width="560" height="315" src="https://www.youtube.com/embed/5qap5aO4i9A" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+  </div>
 </div>
